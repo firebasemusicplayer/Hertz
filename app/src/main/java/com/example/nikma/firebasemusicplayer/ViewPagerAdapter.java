f96e5d1a4c89c2ca8bbbdfaeb5072b5862152c44
nikmakkad08 <nikmakkad08@gmail.com>
2018-05-05 22:02:11 +0530

    Added viewpager

---
+++ app/src/main/java/com/example/nikma/firebasemusicplayer/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.nikma.firebasemusicplayer;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images = {R.drawable.one,R.drawable.two,R.drawable.three,R.drawable.four};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater  = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout,null);
+        ImageView imageView = view.findViewById(R.id.imageview);
+        imageView.setImageResource(images[position]);
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+
+}
