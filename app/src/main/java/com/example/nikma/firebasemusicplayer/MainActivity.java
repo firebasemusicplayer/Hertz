b00b2d8046cbb8b47fb0b5cead78201acf82c9bd
nikmakkad08 <nikmakkad08@gmail.com>
2018-05-05 14:16:45 +0530

    first commit

---
+++ app/src/main/java/com/example/nikma/firebasemusicplayer/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.nikma.firebasemusicplayer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+        private DrawerLayout mlayout;
+        private ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mlayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,mlayout,R.string.open,R.string.close);
+        mlayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
